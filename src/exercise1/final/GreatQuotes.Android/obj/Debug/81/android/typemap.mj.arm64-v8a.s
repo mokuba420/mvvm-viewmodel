ed2955908f0cfd47f2c976cdf79947c1977f0f47
mokuba420 <56065996+mokuba420@users.noreply.github.com>
2019-11-22 13:51:38 +0200

    done

---
+++ src/exercise1/final/GreatQuotes.Android/obj/Debug/81/android/typemap.mj.arm64-v8a.s
@@ -0,0 +1,26 @@
+	/* Data SHA1: 1e6f882c526109b33e4ebc5ff6943d047312459a */
+	.arch	armv8-a
+	.file	"typemap.mj.inc"
+
+	/* Mapping header */
+	.section	.data.mj_typemap,"aw",@progbits
+	.type	mj_typemap_header, @object
+	.p2align	2
+	.global	mj_typemap_header
+mj_typemap_header:
+	/* version */
+	.word	1
+	/* entry-count */
+	.word	1309
+	/* entry-length */
+	.word	262
+	/* value-offset */
+	.word	145
+	.size	mj_typemap_header, 16
+
+	/* Mapping data */
+	.type	mj_typemap, @object
+	.global	mj_typemap
+mj_typemap:
+	.size	mj_typemap, 342959
+	.include	"typemap.mj.inc"
